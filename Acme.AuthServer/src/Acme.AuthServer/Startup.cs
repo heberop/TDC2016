5f3e4634a703f2a6cffdcc458101228cf78f2232
Heber Ortiz Pereira <heber.pereira@lambda3.com.br>
2016-07-06 10:28:22 -0300

    Criação de Clients In-Memory

--- Acme.AuthServer/src/Acme.AuthServer/Startup.cs
+++ Acme.AuthServer/src/Acme.AuthServer/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using IdentityServer4.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
 
@@ -21,6 +23,49 @@ namespace Acme.AuthServer
         {
             //DÊ SEMPRE PREFERÊNCIA POR UM CERTIFICADO REGISTRADO NA MAQUINA
             var cert = new X509Certificate2(Path.Combine(_environment.ContentRootPath, "idsrv3test.pfx"), "idsrv3test");
+
+            var clients = new List<Client>
+            {
+                new Client
+                {
+                    ClientName = "ACME Fabrica",
+                    ClientId = "Acme.Fabrica",
+
+                    RedirectUris = new List<string>
+                    {
+                        "http://localhost:2000"
+                    },
+
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    AllowedScopes = new List<string>
+                    {
+                        StandardScopes.OpenId.Name,
+                        StandardScopes.Profile.Name
+                    }
+                },
+
+                new Client
+                {
+                    ClientName = "ACME Loja",
+                    ClientId = "Acme.Loja",
+
+                    RedirectUris = new List<string>
+                    {
+                        "http://localhost:3000"
+                    },
+
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    AllowedScopes = new List<string>
+                    {
+                        StandardScopes.OpenId.Name,
+                        StandardScopes.Profile.Name
+                    }
+                },
+
+            };
+
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
